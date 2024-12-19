49f79a4c55db45003b960274dae8cec9b31bc2a9
Meysam139000 <meysam.mousavi029@gmail.com>
2024-12-19 12:56:46 +0330

    Update Lidocaine.py

--- Lidocaine.py
+++ Lidocaine.py
@@ -1,5 +1,31 @@
+#python 2.7.15
+import os
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# rang
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+
+
+os.system("clear")
+print(f"{red} ")
 print("ıM QᴀTǝL RᴀVᴀNı")
+print(f"{blue} ")
+print(f"{green} ")
 print("KLR Shad")
+print(f"{red} ")
 print("Member : 1400")
 print("Sik in Binamos Haro Zadam")
 print("Daniel Ryson _ Farmandeh Losifer _ Tailor Mordegan _ Mr Dazai")
